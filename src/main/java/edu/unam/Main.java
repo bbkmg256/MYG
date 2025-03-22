4f32c61b680d076cfb6990f4f5e725cd9b42fb07
bbkmg256 <tecded@proton.me>
2025-03-22 17:25:26 -0300

    Ahora se usa realmente como Main principal del proyecto xd

--- src/main/java/edu/unam/Main.java
+++ src/main/java/edu/unam/Main.java
@@ -1,7 +1,44 @@
 package edu.unam;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import edu.unam.jpaController.*;
+import edu.unam.modelo.Cliente;
+
 public class Main {
 	public static void main(String [] args) {
-		System.out.print("Hola mundo!");
+		// Instancia de la entidad Cliente
+		
+		Cliente c1 = new Cliente(
+				1234, "LAL", "LOLMAN",
+				LocalDate.of(2001, 05, 12), 'M', "LANDIA",
+				"India", 12345, LocalDate.now()
+				);
+		
+		
+		/*
+		Cliente c2 = new Cliente(
+				2222, "fuck", "MAN",
+				LocalDate.of(2002, 06, 14), 'M', "DIA",
+				"Indo", 123, LocalDate.now()
+				);
+			
+		Cliente c3 = new Cliente(
+				2221, "fack", "MeN",
+				LocalDate.of(2002, 06, 14), 'M', "DIA",
+				"Indo", 123, LocalDate.now()
+				);
+		 */
+		
+		// Instancia del JPAController de cliente
+		ClienteJPAController cjpac = new ClienteJPAController();
+		// cjpac.crearCliente(c1);
+		// cjpac.crearCliente(c2);
+		// cjpac.crearCliente(c1);
+		cjpac.eliminarCliente(1234);
+		
+		// Realiza una pequeña devolucion del numero de registros de la tabla correspondiente
+		// cjpac.test();
 	}
 }
