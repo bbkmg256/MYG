b41cd90eb8ca725dd79a12a1a1d844ee946599d9
bbkmg256 <tecded@proton.me>
2025-03-26 17:21:34 -0300

    Modificación del fichero, nada relevante

--- src/main/java/edu/unam/Main.java
+++ src/main/java/edu/unam/Main.java
@@ -3,8 +3,15 @@ package edu.unam;
 import java.time.LocalDate;
 import java.util.List;
 
-import edu.unam.jpaController.*;
 import edu.unam.modelo.Cliente;
+import edu.unam.persistencia.*;
+
+/*
+	NOTA:
+
+	Es un lio administrar el EMF y el EM de los JPAController, DIOOOS!!!
+*/
+
 
 public class Main {
 	public static void main(String [] args) {
@@ -15,15 +22,14 @@ public class Main {
 				LocalDate.of(2001, 05, 12), 'M', "LANDIA",
 				"India", 12345, LocalDate.now()
 				);
-		
-		
-		/*
+				
 		Cliente c2 = new Cliente(
 				2222, "fuck", "MAN",
 				LocalDate.of(2002, 06, 14), 'M', "DIA",
 				"Indo", 123, LocalDate.now()
 				);
 			
+		/*
 		Cliente c3 = new Cliente(
 				2221, "fack", "MeN",
 				LocalDate.of(2002, 06, 14), 'M', "DIA",
@@ -33,11 +39,12 @@ public class Main {
 		
 		// Instancia del JPAController de cliente
 		ClienteJPAController cjpac = new ClienteJPAController();
+		cjpac.crearCliente(c1);
+		cjpac.crearCliente(c2);
 		// cjpac.crearCliente(c1);
-		// cjpac.crearCliente(c2);
-		// cjpac.crearCliente(c1);
-		cjpac.eliminarCliente(1234);
-		
+		// cjpac.eliminarCliente(1234);
+		// cjpac.eliminarCliente(2222);
+		cjpac.cerrarEMF();
 		// Realiza una pequeña devolucion del numero de registros de la tabla correspondiente
 		// cjpac.test();
 	}
