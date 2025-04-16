0987bf0a274da4afde694d71d4ead7bc6dc5a2bf
bbkmg256 <tecded@proton.me>
2025-04-16 01:18:36 -0300

    Modificada entidad, relacionada con entidad Ejercicio (no terminada)

--- src/main/java/edu/unam/modelo/Rutina.java
+++ src/main/java/edu/unam/modelo/Rutina.java
@@ -1,27 +1,55 @@
-
-// Clase rutina
-
 /*
+ * 
+ * CLASE RUTINA
+ * 
+ * ESTABLECE LAS RUTINAS QUE SE CREAN A PARTIR DE LOS EJERCICIOS,
+ * CON SUS SERIES Y REPETICIONES, ASIGNADAS AL ENTRENAMIENTO QUE RECIBE
+ * EL CLIENTE
+ * 
+ */
 
-Establece las rutinas que se crean a partir de los ejercicios, con
-sus series y repeticiones, asignadas al entrenamiento que recibe
-el cliente.
 
-*/
-
-// Paquete
+// PAQUETES
 package edu.unam.modelo;
 
-// Libs
-// import java.util.ArrayList;
+// LIBRERIAS
+// VARIOS
+import java.util.List;
+import java.util.ArrayList;
 
-import jakarta.persistence.Basic; // Modulo JPA para atributos basicos
-import jakarta.persistence.Entity; // Modulo JPA para entidades/objetos
+import jakarta.persistence.Basic; // MODULO JPA PARA ATRIBUTOS BASICOS
+import jakarta.persistence.Entity; // MODULO JPA PARA ENTIDADES/OBJETOS
+import jakarta.persistence.Column;
 
-// Modulos JPA para generacion de ID, valores de generación de ID y forma de generación de ID
+// MODULOS JPA PARA GENERACIONES DE ID, VALORES DE GENERACION DE ID Y FORMA DE GENERACION DE ID
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
+// MODULOS JPA PARA MAPEADO DE RELACIONES
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinTable;
+
+
+/*
+ * 
+ * NOTA:
+ * 
+ * NO DESCOMENTES LAS RELACIONES TODAVIA POR QUE SE ROMPE EL CODIGO
+ * 
+ * LA PERSONALIZADA ES LA RUTINA, ES ELLA LA QUE GUARDA LAS SERIES Y REPETICIONES
+ * NO EL EJERCICIO, UNA RUTINA SE COMPONE DE VARIOS EJERCICIOS Y Y EJERCICIO PUEDE
+ * ESTAR EN VARIAS RUTINAS, POR ENDE, PUEDEN HABER RUTINAS CON EJERICIOS SIMILARES.
+ * 
+ * ES DECIR, ES:
+ * 
+ * VARIOS EJERCICIOS SE RELACIONA CON VARIAS RUTINAS.
+ * 
+ */
+
 
 /**
 *
@@ -29,33 +57,48 @@ import jakarta.persistence.GenerationType;
 */
 @Entity
 public class Rutina {
-	// Atributo
+	// ATIBUTOS
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id_rutina")
 	private int idRutina;
 	
 	@Basic
+	@Column(name = "cantidad_series")
 	private int cantSeries;
+	@Column(name = "cantidad_repeticiones")
 	private int cantRepeticiones;
 	
-	// atributo relacion a clase Ejercicio (Lista)
-	// private ArrayList<Ejercicio> ejercicios = new ArrayList<>();
+	// ATRIBUTO RELACION CON CLASE EJERCICIO
 	
-	// atributo relacion a clase Entrenamiento (Lista)
+	// ATRIBUTO RELACION CON CLASE EJERCICIO
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "ejercicio_id")
+	// private Ejercicio ejercicio;
+	
+	// ATRIBUTO RELACION CON CLASE EJERCICIO (LISTA) [NO TERMINADO]
+	@ManyToMany
+	@JoinTable(
+			name = "rutinas_ejercicios",
+			joinColumns = @JoinColumn(name = "rutina_id"), // COLUMNA ID DE ENTIDAD RUTINA
+			inverseJoinColumns = @JoinColumn(name = "ejercicio_id") // COLUMNA ID DE ENTIDAD EJERCICIO
+	)
+	private List<Ejercicio> ejercicios = new ArrayList<>(); // FK'S DE EJERCICIO
+	
+	// ATRIBUTO RELACION CON CLASE ENTRENAMIENTO (LISTA)
 	// private ArrayList<Entrenamiento> entrenamientos = new ArrayList<>();
 	
-	// Constructores
-	public Rutina() {
-		
-	}
+	// CONTRUCTOR
+	public Rutina() {}
 	
-	public Rutina(int paramIdRutina, int paramCantSeries, int paramCantRepet){
+	public Rutina(int paramIdRutina, int paramCantSeries, int paramCantRepet, List<Ejercicio> listEjercicio){
 		this.idRutina = paramIdRutina;
 		this.cantSeries = paramCantSeries;
 		this.cantRepeticiones = paramCantRepet;
+		this.ejercicios = listEjercicio; // RUTINA "ALMACENA" EJERCICIOS
 	}
 	
-	// Set
+	// SET
 	public void setIdRutina(int valIdRutina){
 		this.idRutina = valIdRutina;
 	}
@@ -68,7 +111,11 @@ public class Rutina {
 		this.cantRepeticiones = valCantRepet;
 	}
 	
-	// Get
+	public void setEjercicio(List<Ejercicio> listEjer) {
+		this.ejercicios = listEjer;
+	}
+	
+	// GET
 	public int getIdRutina(){
 		return this.idRutina;
 	}
@@ -79,5 +126,9 @@ public class Rutina {
 	
 	public int getCantRepeticiones(){
 		return this.cantRepeticiones;
+	}
+	
+	public List<Ejercicio> getEjercicio() {
+		return this.ejercicios;
 	}
 }
