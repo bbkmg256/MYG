f5b09ded7686092c7d0b63ebc72faf8e1335fb68
bbkmg256 <tecded@proton.me>
2025-04-16 01:16:41 -0300

    Modificada entida. relacionada con GM y Rutina (este último no terminado)

--- src/main/java/edu/unam/modelo/Ejercicio.java
+++ src/main/java/edu/unam/modelo/Ejercicio.java
@@ -1,26 +1,62 @@
-
-// Clase ejercicio
-
 /*
+ * 
+ * CLASE EJERCICIO
+ * 
+ * COMPRENDE A LOS EJERCICIOS QUE TRABAJAN UN GRUPO MUSCULAR Y QUE CONFORMARÁN
+ * LA RUTINA QUE SE LE ASIGNA AL CLIENTE.
+ * 
+ */
 
-Comprende a los ejercicio que trabaja un grupo muscular y que conformará
-la rutina que se le asigna al cliente.
 
-*/
-
-//Paquete
+// PAQUETES
 package edu.unam.modelo;
 
-// Libs
-// import java.util.ArrayList;
+// LIBRERIAS
+// VARIOS
+import java.util.ArrayList;
+import java.util.List;
 
-import jakarta.persistence.Basic; // Modulo JPA para atributos basicos
-import jakarta.persistence.Entity; // Modulo JPA para entidades/objetos
+import jakarta.persistence.Basic; // MODULO JPA PARA ATRIBUTOS BASICOS
+import jakarta.persistence.Entity; // MODULO JPA PARA ENTIDADES/OBJETOS
+import jakarta.persistence.Column;
 
-// Modulos JPA para generacion de ID, valores de generación de ID y forma de generación de ID
+// MODULOS JPA PARA GENERACIONES DE ID, VALORES DE GENERACION DE ID Y FORMA DE GENERACION DE ID
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
+// MODULOS JPA DE MAPEADO DE RELACIONES
+import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ManyToMany;
+
+
+/*
+ * 
+ * NOTA:
+ * 
+ * JoinColumn -> SIRVE PARA DETALLAR DATOS REPSECTO A LA COLUMNA DE UAN ENTIDAD
+ * QUE ALMACENA LAS CLAVER FORANEAS DE OTRA ENTIDAD.
+ * (LA ENTIDAD DE MUCHOS SIEMPRE ES LA QUE GUARDA LA FK)
+ * 
+ * fetch -> ESPECIFICA LA FORMA EN QUE SE TOMARÁN LOS DATOS DE ESTA ENTIDAD
+ * DESDE LA BD, PARA NO TRAER TODOS LOS DATOS DE UNA Y SOBRE EXIGIR EL MOTOR.
+ * SE SUPONE QUE LOS DATOS SOLO SE TRAERAN CUANDO SE LLAME AL METODO GET DEL
+ * ATRIBUTO.
+ * 
+ * LA PERSONALIZADA ES LA RUTINA, ES ELLA LA QUE GUARDA LAS SERIES Y REPETICIONES
+ * NO EL EJERCICIO, UNA RUTINA SE COMPONE DE VARIOS EJERCICIOS Y Y EJERCICIO PUEDE
+ * ESTAR EN VARIAS RUTINAS, POR ENDE, PUEDEN HABER RUTINAS CON EJERICIOS SIMILARES.
+ * 
+ * ES DECIR, ES:
+ * 
+ * VARIOS EJERCICIOS SE RELACIONA CON VARIAS RUTINAS.
+ * 
+ */
+
 
 /**
 *
@@ -28,31 +64,46 @@ import jakarta.persistence.GenerationType;
 */
 @Entity
 public class Ejercicio {
-	// Atributo
+	// ATRIBUTOS
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "id_ejercicio")
 	private int idEjercicio;
 	
 	@Basic
+	@Column(name = "nombre_ejercicio")
 	private String nombreEjercicio;
 	
-	// atributo relacion con clase GM
-	// private GrupoMuscular GM;
+	// ATRIBUTO RELACIONADO CON CLASE GM
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "grupo_muscular_id") // ESPECIFICA QUE ESTA COLUMNA ES LA RELACIONAL ENTRE LA TABLA EJERCICIO Y GM
+	private GrupoMuscular GM; // FK'S DE GRUPO MUSCULAR
 	
-	// atributo relacion con clase Rutina
-	// private Rutina rutina;
+	// ATRIBUTO RELACIONADO CON CLASE RUTINA (LISTA)
+	// @OneToMany(mappedBy = "ejercicio", cascade = CascadeType.ALL)
+	// private List<Rutina> rutina = new ArrayList<>();
 	
-	// Constructor
-	public Ejercicio() {
-		
-	}
+	// ATRIBUTO RELACIONADO CON CLASE RUTINA (LISTA) [NO TERMINADO]
+	@ManyToMany(mappedBy = "ejercicios", cascade = CascadeType.ALL)
+	private List<Rutina> rutina = new ArrayList<>();
+
 	
-	public Ejercicio(int paramIdEjer, String paramNombreEjer){
-		this.idEjercicio = paramIdEjer;
+	/* POR EL MOMENTO VOY A RELACIONAR A LA INVERSA DEL DIAGRAMA POR CUESTIONES DE INSTANCIACION (NO TIENE MUCHO SENTIDO COMO VENIA HACIENDO)
+	// ATRIBUTO RELACIONADO CON CLASE RUTINA
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColum(name = "rutina_id")
+	private Rutina rutina; // FK'S DE RUTINA
+	*/
+	
+	// CONSTRUCTOR
+	public Ejercicio() {}
+	
+	public Ejercicio(String paramNombreEjer, GrupoMuscular paramGrupoMuscular){
 		this.nombreEjercicio = paramNombreEjer;
+		this.GM = paramGrupoMuscular; // EJERCICIO "ALMACENA" UN GRUPO MUSCULAR
 	}
 	
-	// Set
+	// SET
 	public void setIdEjercicio(int valIdEjer){
 		this.idEjercicio = valIdEjer;
 	}
@@ -61,12 +112,28 @@ public class Ejercicio {
 		this.nombreEjercicio = valNombreEjer;
 	}
 	
-	// Get
+	public void setGrupoMuscular(GrupoMuscular paramGM) {
+		this.GM = paramGM;
+	}
+	
+	public void setRutinas(List<Rutina> listRutina) {
+		this.rutina = listRutina;
+	}
+	
+	// GET
 	public int getIdEjercicio(){
 		return this.idEjercicio;
 	}
 	
 	public String getNombreEjercicio(){
 		return this.nombreEjercicio;
+	}
+	
+	public GrupoMuscular getGrupoMuscular(){
+		return this.GM;
+	}
+	
+	public List<Rutina> getRutinas(){
+		return this.rutina;
 	}
 }
