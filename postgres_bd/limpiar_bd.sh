1cdc0aa3cd3bc2a135a5a15472f88efa41329b4e
bbkmg256 <tecded@proton.me>
2025-04-20 04:40:38 -0300

    Script para purgar la BD postgres

---
+++ postgres_bd/limpiar_bd.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+# Este script elimina el contenedor y limpia el volumen completo de la BD
+# Usar el caso de quere borrar todo al carajo xd
+
+# ELIMINA EL CONTENEDOR
+docker rm postgres_container
+
+if [ $? -gt 0 ]; then
+	echo -e "[-] Fallo al eliminar el contenedor!\n"
+	exit 1
+fi
+
+# ELIMINA EL VOLUMEN DE ALMACENAMIENTO DEL CONTENEDOR (Y TODAS LAS BD'S)
+docker volume rm postgres_bd_postgres_data
+
+if [ $? -gt 0 ]; then
+	echo -e "[-] Fallo al eliminar el volumen del contenedor!\n"
+	exit 1
+fi
+
+echo -e "[+] Limpieza correcta!\n"
+exit 0
