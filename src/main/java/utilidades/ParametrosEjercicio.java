121087367ac8e9b2f865ef4732f66c42b69e4741
bbkmg256 <tecded@proton.me>
2025-05-03 02:39:15 -0300

    Creado una clase para agregar parámetros dinámicamente al metodo actualizador del servicio de Ejercicio

---
+++ src/main/java/utilidades/ParametrosEjercicio.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ * ESTA CLASE SIRVE COMO UN ALOJADOR DE PARAMETROS PARA INGRESARLO DE FORMA DINAMICA
+ * AL METODO ACTUALIZADOR DE LA ENTIDAD EJERCICIO.
+ * 
+ */
+
+package utilidades;
+
+// LIBRERIAS
+// ENTIDADES
+import edu.unam.modelo.GrupoMuscular;
+
+public class ParametrosEjercicio {
+	public String nombreEjercicio = null;
+	public GrupoMuscular gm = null;
+}
