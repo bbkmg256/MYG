683c3e774f0d851a6d297f504f3c9c398b9d7df2
bbkmg256 <tecded@proton.me>
2025-05-08 16:03:53 -0300

    Cambios menores

--- src/main/java/edu/unam/repositorio/RutinaDAO.java
+++ src/main/java/edu/unam/repositorio/RutinaDAO.java
@@ -24,93 +24,26 @@ public class RutinaDAO{
 	
 	// CREAR ENTIDAD
 	public void crearEntidadRutina(EntityManager em, Rutina entidadRutina) {
-//		manager = emf.createEntityManager();
-//		
-//		try {
-//			manager.getTransaction().begin();
-//			manager.persist(entidadRutina);
-//			manager.getTransaction().commit();			
-//		} catch (Exception e) {
-//			System.out.println(e);
-//			manager.getTransaction().rollback();			
-//		} finally {
-//			manager.close();
-//		}
-		
 		em.persist(entidadRutina);
 	}
 	
 	// LEER ENTIDAD
-	public Rutina obtenerEntidadRutina(EntityManager em, int dni) {
-//		Rutina regEntidad = null;
-//		manager = emf.createEntityManager();
-//			
-//		try {
-//			regEntidad = manager.find(Rutina.class, dni);
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		} finally {
-//			manager.close();
-//		}			
-//		return regEntidad;
-		
+	public Rutina obtenerEntidadRutina(EntityManager em, int dni) {		
 		return em.find(Rutina.class, dni);
 	}
 	
 	// LEER ENTIDADES
 	public List<Rutina> obtenerEntidadesRutina(EntityManager em, String consulta) {
-//		manager = emf.createEntityManager();
-//		String consulta = String.format("SELECT %c FROM %s %c", 'r', "Rutina", 'r'); // Consulta JPQL
-//		System.out.println(consulta);
-//		TypedQuery<Rutina> consultaPreparada;
-//		List<Rutina> regEntidades = null;
-//			
-//		try {
-//			consultaPreparada = manager.createQuery(consulta, Rutina.class);
-//			regEntidades = consultaPreparada.getResultList(); // Devuelve los registros obtenidos en una lista
-//
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		}
-//		return regEntidades;
-		
 		return em.createQuery(consulta, Rutina.class).getResultList();
 	}
 	
 	// ACTUALIZAR ENTIDAD
 	public void actualizarEntidadRutina(EntityManager em, Rutina entidadRutina) {
-//		manager = emf.createEntityManager();
-//		
-//		try {
-//			manager.getTransaction().begin();
-//			manager.merge(entidadRutina);
-//			manager.getTransaction().commit();
-//		} catch (Exception e) {
-//			manager.getTransaction().rollback();
-//			System.out.println(e);
-//		} finally {
-//			manager.close();
-//		}
-		
 		em.merge(entidadRutina);
 	}
 
 	// ELIMINAR ENTIDAD
 	public void eliminarEntidadRutina(EntityManager em, Rutina entidadRutina) {
-//		manager = emf.createEntityManager();
-//
-//		try {
-//			manager.getTransaction().begin();
-//			entidad = manager.merge(entidad); // Reconecta una entidad al gestor de entidades (E.M) que está por fuera del contexto de persistencia
-//			manager.remove(entidad);
-//			manager.getTransaction().commit();
-//		} catch (Exception e) {
-//			manager.getTransaction().rollback();		
-//			System.out.println(e);
-//		} finally {
-//			manager.close();
-//		}
-		
 		em.remove(entidadRutina);
 	}
 }
