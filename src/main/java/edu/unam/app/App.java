b087b8d62c0c156cf11618e1cff6080603fff3a0
bbkmg256 <tecded@proton.me>
2025-05-25 07:20:53 -0300

    Creada nueva clase App (main)

--- src/main/java/edu/unam/app/App.java
+++ src/main/java/edu/unam/app/App.java
@@ -1,44 +1,65 @@
 /*
-	Esta es la clase para las vista en javaFX
+	CLASE PRINCIPAL
 */
 
-package edu.unam.controlador;
+/**
+ *
+ *	@Nombre_del_proyecto: MYG
+ *	@Versión: 0.1.0
+ *	@Autor: BBKMG
+ *
+ */
 
-//JavaFX
+package edu.unam.app;
+
+// LIBRERIAS
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import utilidades.ComprobarConexionBD;
+import utilidades.EMFSingleton;
 import utilidades.RutasVistas;
 
-public class LanzadorVistas extends Application {
-	
-	public LanzadorVistas() {}
-	
+public class App extends Application {
 	@Override
-	public void start(Stage stage) throws Exception {
-		// Carga el fxml desde la ruta ingresada
-//		FXMLLoader carga = new FXMLLoader(this.getClass().getResource(RutasVistas.VISTA_UNO));
+	public void start(Stage ventana) throws Exception {
+		// SOLO SE PAPA EL EMF, PARA QUE FALLE DENTRO DEL METODO AL CREAR EL EM, DANDO SEÑAL DE SI HAY O NO, CONEXION
+		if (!ComprobarConexionBD.hayConexion(EMFSingleton.getInstancia().getEMF())) {
+			// MOMENTANEAMENTE DEJO ESTO ASI, DESPUES LO CAMBIO
+			Alert alerta = new Alert(AlertType.ERROR);
+			alerta.setTitle("Error!");
+			alerta.setHeaderText("ERROR DE CONEXION A BASE DE DATOS!");
+			alerta.setContentText("No se encuentra una base de datos activa!");
+			alerta.showAndWait();
+			System.out.println("[ ERROR ] > No hay conexión a una BD o la misma está caida!");
+			return;
+		}
 		
-		// Crea la estructura raiz con el fxml cargado
-//		AnchorPane esctructuraRaiz = carga.load();
-		
-		// Establece la escena para visualizar
-//		Scene escena = new Scene(esctructuraRaiz);
-//		stage.setScene(escena);
-		
-		
-		FXMLLoader vistaDos = new FXMLLoader(this.getClass().getResource(RutasVistas.VISTA_DOS));
-		AnchorPane raiz = vistaDos.load();
-		Scene escena = new Scene(raiz);
-		stage.setScene(escena);
-		
-		// Muestra la escena
-		stage.show();
+		FXMLLoader cargaVistaInicio = new FXMLLoader(this.getClass().getResource(RutasVistas.VISTA_INICIO)); // LEE EL FICHERO FXML DE LA VISTA
+		AnchorPane raizVistaInicio = cargaVistaInicio.load(); // CARGA EL FICHERO A UN CONTENEDOR RAIZ
+		Scene escenaInicio = new Scene(raizVistaInicio); // CREA UNA NUEVA ESCENA EN BASE AL CONTENEDOR RAIZ
+		ventana.setScene(escenaInicio); // ESTABLECE LA ESCENA EN LA VENTANA
+		ventana.setTitle("Inicio"); // ESTABLECE TITUTO A LA VENTANA
+		ventana.setResizable(false);
+		ventana.show(); // MUESTRA LA VENTANA
 	}
 	
-	public void iniciar() {
+	public static void main(String [] args) {
+		// MENSAJE DE INICIO
+		System.out.println("[ MYG - v0.1.0 ]");
+		System.out.println("by BBKMG");
+		
+		// A ESTE CONTEXTO ME REFIERO (VER CLASE EMFSINGLETON EN PAQUETE UTILIDADES PARA MAS CONTEXTO XD)
+		EMFSingleton.getInstancia().iniciarEMF(); // INICIA CONEXION A BD
+		
+		// LANZA LA INTERFAZ INICIAL
 		launch();
+		
+		// System.out.println("Codigo funcional!");
+		EMFSingleton.getInstancia().cerrarEMF(); // MATA LA CONEXION A BD
 	}
 }
