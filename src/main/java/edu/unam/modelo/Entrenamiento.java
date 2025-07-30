f77a300a634244cb2167ac1b1237b789df043d0f
bbkmg <tecded@proton.me>
2025-07-30 08:47:03 -0300

    Modificación en relacion y creacion de atributo
    Se modificó la política de relación con la entidad rutina, y se agregó
    el atributo "estado".

--- src/main/java/edu/unam/modelo/Entrenamiento.java
+++ src/main/java/edu/unam/modelo/Entrenamiento.java
@@ -54,6 +54,9 @@ import jakarta.persistence.FetchType;
  *	VOY A TENER QUE MODIFICAR LA VISTA DE ENTRENAMIENTO PARA HACER ALGO MEDIO SIMILAR A LA VISTA QUE HAY PARA RUTINA,
  *	DONDE SE CREA EL ENTRENAMIENTO, PARA DESPUES ABRIRLO Y EMPEZAR A CARGAR LAS RUTINAS AL MISMO, ASÍ HAY UN FUJO DE UI
  *	MAS ENTENDIBLE PARA EL USUARIO.
+ *
+ *	EL TUTOR ES POSIBLE QUE SEA NULL, YA QUE SI POR X MOTIVO, SE ELIMINE EL TUTOR O SE DEBA ELIMINAR POR FUERZA MAYOR
+ *	EL ENTRENAMIENTO PASA A ESTAR SIN TUTOR HASTA QUE SE SE ASIGNE UNO NUEVO.
  * 
  */
 
@@ -72,6 +75,7 @@ public class Entrenamiento {
 	
 	@Basic
 	private int puntaje; // PUNTUACION POR PARTE DEL CLIENTE AL TUTOR.
+	private String estado; // PARA DEFINIR SI EL ENTRENAMIENTO ESTÁ EN CURSO O FINALIZÓ.
 
 	@Temporal(TemporalType.DATE)
 	@Column(name = "fecha_inicio")
@@ -86,7 +90,7 @@ public class Entrenamiento {
 	
 	// ATRIBUTO RELACION A CLASE TUTOR
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "tutor_id")
+	@JoinColumn(name = "tutor_id", nullable=true)
 	private Tutor tutor; // FK'S DE TUTOR
 	
 	// ATRIBUTO RELACION A CLASE RUTINA (LISTA)
@@ -99,10 +103,11 @@ public class Entrenamiento {
 	@OneToMany(mappedBy = "entrenamiento")
 	private List<Seguimiento> seguimientos = new ArrayList<>();
 	
+	// ESTA RELACION SI DEBERIA PODER TENER POLITICA DE CASCADA //
 	// ATRIBUTO RELACION A CLASE RUTINAENTRENAMIENTO
-//	@OneToMany(mappedBy = "entrenamiento", cascade = CascadeType.ALL)
-	@OneToMany(mappedBy = "entrenamiento")
-	private List<RutinaEntrenamiento> rutinaentrenamiento = new ArrayList<>();
+//	@OneToMany(mappedBy = "entrenamiento")
+	@OneToMany(mappedBy = "entrenamiento", cascade = CascadeType.ALL)
+	private List<RutinaEntrenamiento> rutinaentrenamientos = new ArrayList<>();
 	
 	// CONTRUCTOR
 	public Entrenamiento() {}
@@ -126,6 +131,7 @@ public class Entrenamiento {
 	) {
 		
 		this.puntaje = 0; // AL INICIO ES 0 HASTA QUE EL CLIENTE LO PUNTUE DESPUES DE 5 SEMANAS DEL ENTRENAMIENTO
+		this.estado = "En curso";
 		this.fechaInicio = paramFechaInicio;
 		this.fechaFin = paramFechaFin;
 		this.cliente = paramCli;
@@ -167,7 +173,11 @@ public class Entrenamiento {
 //	}
 	
 	public void setRutinaEntrenamiento(List<RutinaEntrenamiento> paramRE) {
-		this.rutinaentrenamiento = paramRE;
+		this.rutinaentrenamientos = paramRE;
+	}
+	
+	public void setEstado(String valorEstado) {
+		this.estado = valorEstado;
 	}
 	
 	// GET
@@ -203,7 +213,11 @@ public class Entrenamiento {
 //		return this.rutina;
 //	}
 	
-	public List<RutinaEntrenamiento> getRutinaEntrenamiento() {
-		return this.rutinaentrenamiento;
+	public List<RutinaEntrenamiento> getRutinaEntrenamientos() {
+		return this.rutinaentrenamientos;
+	}
+	
+	public String getEstado() {
+		return this.estado;
 	}
 }
